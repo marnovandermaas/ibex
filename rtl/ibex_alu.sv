7032ab749bd2cebb24f9f580a6f9a7d26582aeaf
Ivan Ribeiro <ivanmgribeiro@gmail.com>
2019-07-03 15:10:45 +0100

    Clean up generate blocks slightly
    Changed the way the generate blocks are wrapped with ifdef
    statements in order to make it easier to pull changes from the original
    ibex repository in future.

--- rtl/ibex_alu.sv
+++ rtl/ibex_alu.sv
@@ -44,20 +44,19 @@ module ibex_alu (
   logic [32:0] operand_b_neg;
 
   // bit reverse operand_a for left shifts and bit counting
+
   `ifdef QUARTUS
     generate
-      genvar k;
-      for (k = 0; k < 32; k++) begin : gen_rev_operand_a
-        assign operand_a_rev[k] = operand_a_i[31-k];
-      end
-    endgenerate
-  `else
-    for (genvar k = 0; k < 32; k++) begin : gen_rev_operand_a
-      assign operand_a_rev[k] = operand_a_i[31-k];
-    end
   `endif
 
+  genvar k;
+  for (k = 0; k < 32; k++) begin : gen_rev_operand_a
+    assign operand_a_rev[k] = operand_a_i[31-k];
+  end
 
+  `ifdef QUARTUS
+    endgenerate
+  `endif
 
   ///////////
   // Adder //
@@ -138,15 +137,15 @@ module ibex_alu (
   // bit reverse the shift_right_result for left shifts
   `ifdef QUARTUS
     generate
-      genvar j;
-      for (j = 0; j < 32; j++) begin : gen_rev_shift_right_result
-        assign shift_left_result[j] = shift_right_result[31-j];
-      end
+  `endif
+
+  genvar j;
+  for (j = 0; j < 32; j++) begin : gen_rev_shift_right_result
+    assign shift_left_result[j] = shift_right_result[31-j];
+  end
+
+  `ifdef QUARTUS
     endgenerate
-  `else
-    for (genvar j = 0; j < 32; j++) begin : gen_rev_shift_right_result
-      assign shift_left_result[j] = shift_right_result[31-j];
-    end
   `endif
 
   assign shift_result = shift_left ? shift_left_result : shift_right_result;
